f37e92d44edfcf9ae258ce4abf484033c0d57f89
Marcelearn <jaworskimarcel@gmail.com>
2022-12-13 15:17:00 +0100

    bug fixed

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function testFunction(x, y) {
+function testFunction(x, y, z) {
     if(!Number.isInteger(x)) return false;
     if(x === undefined) return false;
     if(x && y === undefined) return x * x;
+    if(arguments.length > 2) {
+        let result = Array.from(arguments).reduce(
+            (acc, cur) => acc * cur, 1
+            );
+        return result;
+    }  
     if(x && y) return x * y;
 }
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -15,9 +15,13 @@ test("gibt einArgument, die mulpliziert sichselbst", () => {
 })
 //3. Ist das Argument keine ganze Zahl (Integer), soll "false" zurückgegeben werden.
 test("ist einArgument Integer", () => {
-    expect(testFunction(1.2)).toBe(false)
+    expect(testFunction(3.2)).toBe(false)
 })
 //4. Werden zwei Argumente übergeben, sollen sie multipliziert und das Ergebnis zurückgegeben werden (a * b).
 test("gibt zwei Argument? multiplizert beides", () => {
     expect(testFunction(3, 2)).toBe(6)
+})
+//5. Werden beliebig viele Argumente übergeben, sollen sie alle multipliziert und das Ergebnis zurückgegeben werden (a * b * c * ...).
+test("mehr als zwei Argument? multipliziert alles", () => {
+    expect(testFunction(2, 2, 2)).toBe(8)
 })
